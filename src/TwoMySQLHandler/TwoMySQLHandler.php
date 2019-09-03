01664d7f09b6f968367a54b2e4e5739ce6f7aa63
Fred Trotter <fred.trotter@gmail.com>
2019-09-03 08:47:22 -0500

    so there

--- src/TwoMySQLHandler/TwoMySQLHandler.php
+++ src/TwoMySQLHandler/TwoMySQLHandler.php
@@ -28,9 +28,14 @@ class TwoMySQLHandler extends AbstractProcessingHandler
     protected $pdo;
 
     /**
-     * @var PDOStatement statement to insert a new record
+     * @var PDOStatement statement to insert a new message record
      */
     private $statement;
+
+    /**
+     * @var PDOStatement statement to insert a new context record
+     */
+    private $context_statement;
 
     /**
      * @var string the table to store the log messages
@@ -51,7 +56,7 @@ class TwoMySQLHandler extends AbstractProcessingHandler
     /**
      * @var array default fields that are stored in db
      */
-    private $defaultfields = array('id', 'channel', 'level', 'message', 'time');
+    private $defaultfields = array('id', 'channel', 'level', 'message', 'created_at');
 
     /**
      * @var string[] additional fields to be stored in the database
@@ -91,7 +96,7 @@ class TwoMySQLHandler extends AbstractProcessingHandler
         }
         $this->message_table = $message_table;
         $this->context_table = $context_table;
-        $this->log_databaes = $log_db;
+        $this->log_database = $log_db;
         $this->additionalFields = $additionalFields;
         parent::__construct($level, $bubble);
     }
@@ -101,19 +106,38 @@ class TwoMySQLHandler extends AbstractProcessingHandler
      */
     private function initialize()
     {
-        $this->pdo->exec(
-            'CREATE TABLE IF NOT EXISTS `'.$this->log_database.'`.`'.$this->message_table.'` '
-            .'(id BIGINT(20) NOT NULL AUTO_INCREMENT PRIMARY KEY, channel VARCHAR(255), level INTEGER, message LONGTEXT, time INTEGER UNSIGNED, INDEX(channel) USING HASH, INDEX(level) USING HASH, INDEX(time) USING BTREE)'
-        );
+     
+       $create_message_table_sql = "
+CREATE TABLE IF NOT EXISTS `$this->log_database`.`$this->message_table` (
+	id BIGINT(20) NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+	channel VARCHAR(255), 
+	level INTEGER, 
+	message LONGTEXT, 
+	created_at datetime DEFAULT CURRENT_TIMESTAMP(),
+	INDEX(channel), 
+	INDEX(level), 
+	INDEX(created_at)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;  ";
 
-        $this->pdo->exec(
-            'CREATE TABLE IF NOT EXISTS `'.$this->log_database.'`.`'.$this->context_table.'` '
-            .'(id BIGINT(20) NOT NULL AUTO_INCREMENT PRIMARY KEY, message_id BIGINT(20), channel VARCHAR(255), level INTEGER, message LONGTEXT, time INTEGER UNSIGNED, INDEX(channel) USING HASH, INDEX(level) USING HASH, INDEX(time) USING BTREE)'
-        );
+       $this->pdo->exec($create_message_table_sql);
+
+	$create_context_table_sql = "
+CREATE TABLE IF NOT EXISTS `$this->log_database`.`$this->context_table` (
+	id BIGINT(20) NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+	message_id BIGINT(20), 
+	context_key VARCHAR(255),
+	context_value LONGTEXT,
+	INDEX(message_id),
+	INDEX(context_key)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;	
+";
+
+
+        $this->pdo->exec( $create_context_table_sql );
 
         //Read out actual columns
         $actualFields = array();
-        $rs = $this->pdo->query('SELECT * FROM `'.$this->message_table.'` LIMIT 0');
+        $rs = $this->pdo->query('SELECT * FROM `'.$this->log_database.'`.`'.$this->message_table.'` LIMIT 0');
         for ($i = 0; $i < $rs->columnCount(); $i++) {
             $col = $rs->getColumnMeta($i);
             $actualFields[] = $col['name'];
@@ -169,8 +193,10 @@ class TwoMySQLHandler extends AbstractProcessingHandler
             $fields .= ", :$f";
         }
 
+	$sql_to_prepare = 'INSERT INTO `'.$this->log_database.'`.`' . $this->message_table . '` (' . $columns . ') VALUES (' . $fields . ')';
+	
         $this->statement = $this->pdo->prepare(
-            'INSERT INTO `'.$this->log_database.'`.`' . $this->message_table . '` (' . $columns . ') VALUES (' . $fields . ')'
+		$sql_to_prepare
         );
     }
 
@@ -192,45 +218,53 @@ class TwoMySQLHandler extends AbstractProcessingHandler
          */
         $this->fields = $this->defaultfields;
 
-        /*
-         * merge $record['context'] and $record['extra'] as additional info of Processors
-         * getting added to $record['extra']
-         * @see https://github.com/Seldaek/monolog/blob/master/doc/02-handlers-formatters-processors.md
-         */
-        if (isset($record['extra'])) {
-            $record['context'] = array_merge($record['context'], $record['extra']);
-        }
+	$created_at = date('Y-m-d H:i:s');
 
         //'context' contains the array
-        $contentArray = array_merge(array(
+        $contentArray = [
                                         'channel' => $record['channel'],
                                         'level' => $record['level'],
                                         'message' => $record['message'],
-                                        'time' => $record['datetime']->format('U')
-                                    ), $record['context']);
+                                        'created_at' => $created_at,
+			];                         
 
-        // unset array keys that are passed put not defined to be stored, to prevent sql errors
-        foreach($contentArray as $key => $context) {
-            if (! in_array($key, $this->fields)) {
-                unset($contentArray[$key]);
-                unset($this->fields[array_search($key, $this->fields)]);
-                continue;
-            }
-
-            if ($context === null) {
-                unset($contentArray[$key]);
-                unset($this->fields[array_search($key, $this->fields)]);
-            }
-        }
 
         $this->prepareStatement();
 
         //Fill content array with "null" values if not provided
-        $contentArray = $contentArray + array_combine(
-            $this->additionalFields,
-            array_fill(0, count($this->additionalFields), null)
-        );
 
         $this->statement->execute($contentArray);
+
+	$message_id = $this->pdo->lastInsertId(); //should I be using a 'name' argument? I think so...
+
+	
+	foreach($record['context'] as $context_key =>  $context_value){
+		$this->saveContextRow($message_id, $context_key, $context_value);
+	}		
+
     }
+
+	protected function saveContextRow($message_id, $context_key, $context_value){
+
+		if(is_null($this->context_statement)){
+
+			$sql_to_prepare = "
+INSERT INTO `$this->log_database`.`$this->context_table` (
+	message_id, context_key, context_value 
+	) VALUES (
+	:message_id, :context_key, :context_value
+)";
+	
+      	  		$this->context_statement = $this->pdo->prepare(
+				$sql_to_prepare
+        		);
+		} 
+
+		//no matter what here we should have the prepared statement ready..
+		//most of the time it will just be this..
+		$this->context_statement->execute(['message_id' => $message_id, 'context_key' => $context_key, 'context_value' => $context_value]);
+	
+	
+	}
+
 }
